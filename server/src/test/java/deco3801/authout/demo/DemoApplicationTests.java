271017b858f73667109b6d8656be3b29a128b9a9
Evan Hughes <ehugh1@gmail.com>
2018-08-06 17:07:48 +1000

    [Setup] Added demo server
    - Added jenkins build for the server

---
+++ server/src/test/java/deco3801/authout/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package deco3801.authout.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
