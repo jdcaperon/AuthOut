60ae36677aad711bb66bb24e8034f9e9c89fc95a
Ryan <ryankurz123@gmail.com>
2018-08-17 09:08:59 +1000

    working with face detection

--- android/AuthOut/app/src/main/java/rocketpotatoes/authout/MainActivity.java
+++ android/AuthOut/app/src/main/java/rocketpotatoes/authout/MainActivity.java
@@ -1,25 +1,36 @@
 package rocketpotatoes.authout;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.*;
 import com.camerakit.CameraKitView;
-import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private CameraKitView cameraKitView;
+    private FaceDetector faceDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fresco.initialize(getApplicationContext());
 
         setContentView(R.layout.activity_main);
 
         cameraKitView = findViewById(R.id.cameraKitView);
+        faceDetector = new FaceDetector.Builder(this)
+                .setTrackingEnabled(true)
+                .build();
     }
 
     public void onCaptureButtonClicked(View view) {
@@ -27,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onImage(CameraKitView cameraKitView, byte[] bytes) {
                 Log.i("MainActivity", "Image captured");
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+
+
+                Frame outputFrame = new Frame.Builder().setBitmap(bmp).build();
+                SparseArray<Face> sparseArray = faceDetector.detect(outputFrame);
+                try {
+                    Toast.makeText(MainActivity.this, "Detected " + sparseArray.size(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+
+                    Toast.makeText(MainActivity.this, sparseArray.get(0).toString() , Toast.LENGTH_SHORT).show();
+                }
+
+
+
             }
         });
     }
