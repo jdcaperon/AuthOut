59a15083f65c2be49c3a34d5d99703e6822695e1
jdcaperon <jack.d.caperon@gmail.com>
2018-09-01 18:46:58 +1000

    Init adapter

---
+++ android/AuthOut/app/src/main/java/rocketpotatoes/authout/ChildSelectorAdapter.java
@@ -0,0 +1,28 @@
+package rocketpotatoes.authout;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+class ChildSelectorAdapter extends RecyclerView.Adapter {
+
+    public ChildSelectorAdapter(Object p0) {
+        
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- android/AuthOut/app/src/main/java/rocketpotatoes/authout/SelectStudentActivity.java
+++ android/AuthOut/app/src/main/java/rocketpotatoes/authout/SelectStudentActivity.java
@@ -3,6 +3,8 @@ package rocketpotatoes.authout;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class SelectStudentActivity extends AppCompatActivity {
@@ -10,6 +12,18 @@ public class SelectStudentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView mChildSelectorView = (RecyclerView) findViewById(R.id.child_selector);
+
+        mChildSelectorView.setHasFixedSize(true);
+        mChildSelectorView.setLayoutManager(layoutManager);
+
+
+
+        ChildSelectorAdapter mChildSelectorAdapter = new ChildSelectorAdapter();
+        mChildSelectorView.setAdapter(mChildSelectorAdapter);
+
         setContentView(R.layout.activity_select_student);
     }
 
