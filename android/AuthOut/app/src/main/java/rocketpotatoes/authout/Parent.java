afe38502f58bc3441cafd5066b90fa79bf97b61b
Ryan <ryankurz123@gmail.com>
2018-09-01 18:23:00 +1000

    added function for button logic

--- android/AuthOut/app/src/main/java/rocketpotatoes/authout/Parent.java
+++ android/AuthOut/app/src/main/java/rocketpotatoes/authout/Parent.java
@@ -3,17 +3,19 @@ package rocketpotatoes.authout;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import rocketpotatoes.authout.DynamicButtonOptions;
 
 public class Parent {
     private final String firstName;
     private final String surname;
     private final List<Child> children;
 
-    /**
+    /** A parent object in order to
      *
      * @param firstName - first name of the parent
      * @param surname   - surname of the parent
-     * @param children - Array of Maps mapping
+     * @param children - Array of Maps of child objects with keys
+     *                   'firstName', 'surname', and 'status'
      */
     public Parent(String firstName, String surname, Map<String, Object>[] children) {
         this.children = new ArrayList<>();
@@ -42,5 +44,32 @@ public class Parent {
         return children;
     }
 
+    public void updateButtonOptions(Parent parent) {
+        parent = null; //todo actual parent implementation or initiation
+
+        List<Child> children = parent.getChildren();
+        getOptionByChildren(children);
+
+    }
+
+
+    public Enum getOptionByChildren(List<Child> children) {
+        String signedInText = "Signed-In";
+        if (children.size() == 0) return DynamicButtonOptions.NOT_COMPATIBLE;
+        if (children.size() == 1) {
+            if (children.get(0).getStatus().equals(signedInText)) {
+                return DynamicButtonOptions.SIGN_IN;
+            }
+            return DynamicButtonOptions.SIGN_OUT;
+        } else {
+            for (int i = 0; i < children.size() - 1; i++) {
+                if (!children.get(i).getStatus().equals(children.get(i + 1).getStatus())) {
+                    return DynamicButtonOptions.NOT_COMPATIBLE;
+                }
+            }
+        }
+        return children.get(0).getStatus().equals(signedInText) ?
+                DynamicButtonOptions.SIGN_IN : DynamicButtonOptions.SIGN_OUT;
+    }
 
 }
