04e61e1ad8cecce1d57583e30c903acf6513226a
jdcaperon <jack.d.caperon@gmail.com>
2018-09-02 19:25:17 +1000

    Added checkboxes

--- android/AuthOut/app/src/main/java/rocketpotatoes/authout/ChildSelectorAdapter.java
+++ android/AuthOut/app/src/main/java/rocketpotatoes/authout/ChildSelectorAdapter.java
@@ -5,20 +5,32 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.List;
 
 class ChildSelectorAdapter extends RecyclerView.Adapter<ChildSelectorAdapter.MyViewHolder> {
 
+
     private List<Child> childList;
+    private int selectedChild = -1;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView name;
+        public CheckBox checkbox;
 
         public MyViewHolder(View view) {
             super(view);
             name = (TextView) view.findViewById(R.id.childlist_name);
+            checkbox = (CheckBox) view.findViewById(R.id.child_select);
+            checkbox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectedChild = getAdapterPosition();
+                    notifyDataSetChanged();
+                }
+            });
         }
     }
 
@@ -30,6 +42,7 @@ class ChildSelectorAdapter extends RecyclerView.Adapter<ChildSelectorAdapter.MyV
     public void onBindViewHolder(MyViewHolder holder, int position) {
         Child child = childList.get(position);
         holder.name.setText(child.getFirstName());
+        holder.checkbox.setChecked(position == selectedChild);
     }
 
     @NonNull
