812a78c949588ea1872a3ae1478cd8876c47e919
Jordan Holder <jholder691@gmail.com>
2018-09-11 07:19:53 +1000

    Fixing icons

--- web/src/php/nav.php
+++ web/src/php/nav.php
@@ -9,8 +9,8 @@
     <ul class="navbar-nav ">
         <li class ="nav-item"><a class="nav-link"href="index.php"><i class="material-icons lighter">home</i>Home</a></li>
         <li class ="nav-item"><a class="nav-link"href="statistics.php"><i class="material-icons lighter">assessment</i>Statistics</a></li>
-        <li class ="nav-item"><a class="nav-link"href="live_monitor.php"><i class="material-icons lighter">assignment</i>Live Monitor</a></li>
-        <li class ="nav-item"><a class="nav-link"href="report_generator.php"><i class="material-icons lighter">wifi_tethering</i>Report Generator</a></li>
+        <li class ="nav-item"><a class="nav-link"href="live_monitor.php"><i class="material-icons lighter">wifi_tethering</i>Live Monitor</a></li>
+        <li class ="nav-item"><a class="nav-link"href="report_generator.php"><i class="material-icons lighter">assignment</i>Report Generator</a></li>
         <li class ="nav-item"><a class="nav-link"href="help.php"><i class="material-icons lighter">help</i>Help</a></li>
         <li class ="nav-item"><a class="nav-link"href="#"><i class="material-icons lighter">exit_to_app</i>Log out</a></li>
     </ul>
