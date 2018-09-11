c526254e6a7ea6732cc339fc39c1808257b26f2f
Jordan Holder <jholder691@gmail.com>
2018-09-11 20:35:19 +1000

    Start of work on login page

--- web/src/index.php
+++ web/src/index.php
@@ -1,13 +1,24 @@
 <?php
     $stylesheets = array("css/index_style.css");
     include('php/head.php');
-    include('php/nav.php');
 
 ?>
     <body>
-        <div class="page_content">
-
-
+		<header>
+			<img src="img/placeholder.png" alt="logo" id="login-logo">
+			<h1>Login</h1>
+		</header>
+        <div class="login_window">			
+			<i id = "login-icon" class="material-icons">account_circle</i>
+			
+			<form id="login-form" class="container">
+				<span><label for="username">Username:</label><input type="text" name="username" id="username"></span><br>
+				<span><label for="password">Password:</label><input type="text" name="password" id="password"></span>
+			</form>
+			
+			<button type="button" class="rounded_button blue_button">LOGIN</button>
+			<button type="button" class="rounded_button purple_button">DON'T HAVE AN ACCOUNT?</button>
+		</div>
         <?php include 'php/footer_scripts.php'?>
         <script src="js/index_script.js" rel="javascript" type="text/javascript"></script>
 
--- web/src/php/footer_scripts.php
+++ web/src/php/footer_scripts.php
@@ -3,19 +3,4 @@
 <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<?php
-$mapsAPIKey = "AIzaSyB4BQsFibXn3CW33vNlggzbj3QbJdHgxFc";
-
-	// Map include is different for game details page because it needs to callback function
-	if (strpos($_SERVER['REQUEST_URI'], 'game_details.php') === false) {
-		echo "<script src='https://maps.googleapis.com/maps/api/js?key=$mapsAPIKey' async defer></script>";
-	} else {
-		echo "<script src='https://maps.googleapis.com/maps/api/js?key=$mapsAPIKey&callback=initMap' async defer></script>";
-	}
-
-?>
-<script type="text/javascript" src="libs/jqwidgets/jqxcore.js"></script>
-<script type="text/javascript" src="libs/jqwidgets/jqxnotification.js"></script>
-<script src='libs/spectrum/spectrum.js'></script>
 <script src="js/main.js" rel="javascript" type="text/javascript"></script>
-<script type="text/javascript" src="js/account_setup_script.js"></script>
