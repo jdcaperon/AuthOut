2a45a320df7e413a9df1638800090f91d868c0b8
Evan Hughes <ehugh1@gmail.com>
2018-09-15 00:29:48 +1000

    [Server] Fleshing out views

---
+++ server/views/Child.py
@@ -0,0 +1,11 @@
+import functools
+from flask import Blueprint, jsonify
+from models.HeartbeatModel import HeartbeatModel
+
+bp = Blueprint('heartbeat', __name__, url_prefix="/child")
+
+
+@bp.route('/')
+def heartbeat():
+  data: HeartbeatModel = HeartbeatModel()
+  return jsonify(data.__dict__)
--- server/views/Heartbeat.py
+++ server/views/Heartbeat.py
@@ -1,6 +1,7 @@
 import functools
 from flask import Blueprint, jsonify
 from models.HeartbeatModel import HeartbeatModel
+from .Auth import requires_auth, requires_access_level
 
 bp = Blueprint('heartbeat', __name__, url_prefix="/heartbeat")
 
@@ -9,3 +10,13 @@ bp = Blueprint('heartbeat', __name__, url_prefix="/heartbeat")
 def heartbeat():
   data: HeartbeatModel = HeartbeatModel()
   return jsonify(data.__dict__)
+
+@bp.route("/auth")
+@requires_auth
+def auth_test():
+  return "hello"
+
+@bp.route("/permission/kiosk")
+@requires_access_level("kiosk")
+def permission_kiosk():
+  return "Youve got kiosk permissions"
