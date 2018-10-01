f18072b47bbd4bcfc194127326ba9bc3c90c77bf
Jordan Holder <jholder691@gmail.com>
2018-10-01 19:54:57 +1000

    Added help content

--- web/src/php/help.php
+++ web/src/php/help.php
@@ -2,14 +2,19 @@
 <html lang="en">
 	<?php 
 		$pageName = "Help";
-		//$stylesheets = array("help_style.css");
+		$stylesheets = array("help_style.css");
 		include_once $_SERVER['DOCUMENT_ROOT'].'/php/head.php';
 		include $_SERVER['DOCUMENT_ROOT'].'/html/nav.html';
 		
 	?>
 	
 	<body>
-		<p style="padding-top: 120px"><?php echo $pageName ?> contnet here!</p>
+		<div id="help-content">
+			<h2>How can we help?</h2>
+			<button type="button" id="login-button" class="rounded_button purple_button">How to use the site</button>
+			<button type="button" id="login-button" class="rounded_button purple_button">Report a bug</button>
+			<button type="button" id="login-button" class="rounded_button purple_button">Contact us</button>
+		</div>
 	</body>
 	
 	<?php 
