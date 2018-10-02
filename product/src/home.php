291b710eb9e23502b4569399c7a339c0c6dd883d
Katie <katherine.gray@uqconnect.edu.au>
2018-10-02 10:51:24 +1000

    product website start

---
+++ product/src/home.php
@@ -0,0 +1,10 @@
+<?php
+	$stylesheets = array("css/home_style.css");
+    include('php/head.php');
+	include('php/nav.php');
+?>
+    <body>
+        <?php include 'php/footer_scripts.php'?>
+
+    </body>
+</html>
---
+++ product/src/php/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark fixed-top navbar-default navbar-fixed-top">
+	<div class="container-fluid" id="div1">
+	<ul class="nav ">
+		<li class="nav-item"><a class="nav-link"href="#overview">Overview</a></li>
+		<li class="nav-item"><a class="nav-link"href="#features">Features</a></li>
+		<li class="nav-item"><a class="nav-link"href="#mail">Mailing List</a></li>
+	</ul>
+	 <img class="nav navbar-nav navbar-right" src="img/logo.png" alt="AuthOut Logo" id="logo"/>
+
+
+	</div>
+</nav>
+
+<!-- <nav class="navbar navbar-expand navbar-dark bg-dark fixed-top navbar-default navbar-fixed-top">
+    <div >
+         <img class="navbar-brand navbar-nav ml-auto" src="img/logo.png" alt="AuthOut Logo" id="logo"/>
+		 <ul class="navbar-nav">
+			<li class="nav-item"><a class="nav-link"href="#overview">Overview</a></li>
+			<li class="nav-item"><a class="nav-link"href="#features">Features</a></li>
+			<li class="nav-item"><a class="nav-link"href="#mail">Mailing List</a></li>
+		 </ul>
+    </div>
+
+</nav> -->
