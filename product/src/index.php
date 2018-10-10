540694723960c9bac6356f4b593dfd3a9f59c2e6
Katie <katherine.gray@uqconnect.edu.au>
2018-10-10 15:54:03 +1000

    link sharing and styling

--- product/src/index.php
+++ product/src/index.php
@@ -16,18 +16,20 @@
 	include('php/nav.php');
 ?>
     <body>
-		<div id ="scroll">
+
+		<div id ="scroll" data-spy="scroll" data-target=".navbar" data-offset="50">
 			<div id="content">
 				<div class = "page">
 					<div id="overview">
 						<h3>Overview</h3>
-
 							<div id="overview-1" >
 							<p>AuthOut uses facial recognition technologies to track attendance in childcare centres.</p>
 							</div>
 							<div id="overview-2" >
 								<iframe src="https://www.youtube.com/embed/phisEhrYKUI?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 							</div>
+							<p></p>
+
 
 					</div>
 				</div>
@@ -38,8 +40,7 @@
 						<div class="container">
 							<div class="row">
 								<div class="col-sm">
-									<br><br><br>
-									<p>We've embraced progressive design techniques and technologies in creating AuthOut. </p><br><br>
+									<p class="paragraph-margin">We've embraced progressive design techniques and technologies in creating AuthOut. </p>
 									<p>Our product features:</p>
 									<ul>
 										<li>Easy Setup Kiosk</li>
@@ -49,9 +50,12 @@
 										<li>Admin Web Portal</li>
 										<li>24-7 support</li>
 									</ul>
+									<div class="addthis_inline_share_toolbox paragraph-margin">Share this page!</div>
 								</div>
-								<div class="col-sm">
+								<div class="col-sm" id ="carousel-container">
+
 									<div id="carousel" class="carousel slide" data-ride="carousel">
+
 										<div class="carousel-inner">
 											<div class="carousel-item active">
 											  <img class="d-block w-100" src="img/ryan.png" alt="Ryan">
@@ -62,15 +66,16 @@
 											<div class="carousel-item">
 											  <img class="d-block w-100" src="img/done.png" alt="Third slide">
 											</div>
-
+											<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+											            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+											            <span class="sr-only">Previous</span>
+											</a>
+											<a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+											            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+											            <span class="sr-only">Next</span>
+											</a>
 										</div>
-										<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-										<span class="sr-only">Previous</span>
-										</a>
-										<a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-										<span class="carousel-control-next-icon" aria-hidden="true"></span>
-										<span class="sr-only">Next</span>
+
 										</a>
 									</div>
 								</div>
@@ -85,10 +90,13 @@
 					<div id="mail">
 						<h3 >Purchase</h3>
 						<div id="mail-content">
-							<p>Our product is still under development. Sign up to our mailing list to receive AuthOut status updates.</p>
-							<form id="email-form">
+							<p>Our product is still under development.</p>
+								<form id="email-form">
 								<div class="form-group">
-								    <label >Email address</label>
+									<p> Sign up to our mailing list to receive AuthOut status updates.</p>
+								</div>
+								<div class="form-group">
+								    <label>Email address</label>
 								    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email" required>
 								    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 							  	</div>
@@ -122,8 +130,8 @@
 
         <?php include 'php/footer_scripts.php'?>
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+		<!-- www.addthis.com/dashboard Sharing tools -->
+		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5bbd909968418185"></script>
 
     </body>
 </html>
