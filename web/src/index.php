f7362d01cd39076939609c34f79713bdb1fd7952
Katie <katherine.gray@uqconnect.edu.au>
2018-10-19 15:25:56 +1000

    login modal

--- web/src/index.php
+++ web/src/index.php
@@ -26,7 +26,7 @@
 
 					<button type="button" id="login-button" class="rounded_button blue_button">LOG IN</button>
 					<!-- TODO: add functionality  -->
-					<button type="button" class="rounded_button purple_button">DON'T HAVE AN ACCOUNT?</button>
+					<button type="button" class="rounded_button purple_button" data-toggle="modal" data-target="#noAccountModal">DON'T HAVE AN ACCOUNT?</button>
 				</div>
 			</div>
 		</body
@@ -39,3 +39,25 @@
     </body>
 
 </html>
+
+<!-- Modal -->
+<div class="modal fade" id="noAccountModal" tabindex="-1" role="dialog" >
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">No Account?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+		  <br>
+        <p> Please talk to your manager - they'll be able to help you out. If you are the manager, check your startup guide for account information!</p>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn rounded_button blue_button" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
