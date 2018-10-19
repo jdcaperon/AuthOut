e7701361c3e531b7e8ec436aefa09f81d9996565
Katie <katherine.gray@uqconnect.edu.au>
2018-10-19 15:36:55 +1000

    sign out working

--- web/src/php_inclusions/nav.php
+++ web/src/php_inclusions/nav.php
@@ -24,7 +24,7 @@
         <div class="col-md-11" >
 			<img  src="img/authout_full_logo.png" alt="AuthOut Logo" id="logo"/>
 		</div>
-			<a class="col-md-1  text-nowrap">Log Out</a>
+			<a id="signout" class="col-md-1 text-nowrap">Log Out</a>
 
 
 </nav>
