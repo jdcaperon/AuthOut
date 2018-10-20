32321c22f1068dfd1c98082f62b8ff2b80057321
Evan Hughes <ehugh1@gmail.com>
2018-10-20 16:32:18 +1000

    Need to handle no username so it doesnt prompt

--- web/src/js/index_script.js
+++ web/src/js/index_script.js
@@ -3,22 +3,29 @@ $(document).ready(function() {
 		event.preventDefault();
 		var username = $("#username").val();
 		var password = $("#password").val();
-		$.ajax
-		({
-			type: "GET",
-			url: "https://deco3801.wisebaldone.com/api/login",
-			dataType: 'json',
-			async: false,
-			username: username,
-			password: password,
-			data: '{ "comment" }',
-			success: function (){
-				window.location.href = "live.php"; 
-			},
-			fail: function() {
-				console.log("Failed");
-			}
-		});
+
+		if (username == "") {
+			// notify user somehow
+			console.log("Need a username");
+		} else {
+			$.ajax
+			({
+				type: "GET",
+				url: "https://deco3801.wisebaldone.com/api/login",
+				dataType: 'json',
+				async: false,
+				username: username,
+				password: password,
+				data: '{ "comment" }',
+				success: function (){
+					console.log("Success");
+					window.location.href = "live.php"; 
+				},
+				error: function() {
+					console.log("Failed");
+				}
+			});
+		}
 	});
 	
 });
