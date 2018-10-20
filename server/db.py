2bfd7e26839c5f7875384b5334edc6aa65e0f041
Evan Hughes <ehugh1@gmail.com>
2018-10-20 14:13:00 +1000

    Added Admins

--- server/db.py
+++ server/db.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 import os
@@ -7,5 +8,5 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+CORS(app)
 
