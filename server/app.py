d889dcf37432257218cce3540295034cc2b20609
Evan Hughes <ehugh1@gmail.com>
2018-10-20 15:01:09 +1000

    Added a login endpoint

--- server/app.py
+++ server/app.py
@@ -1,13 +1,24 @@
+from flask import Response
+from views.Auth import requires_auth
+
 from db import app
 import views
 
 # Register sub views.
+
 app.register_blueprint(views.Heartbeat.bp)
 app.register_blueprint(views.Kiosk.bp)
 app.register_blueprint(views.Parent.bp)
 app.register_blueprint(views.Child.bp)
 app.register_blueprint(views.Entry.bp)
 app.register_blueprint(views.Admin.bp)
+
+
+@app.route('/login')
+@requires_auth
+def login():
+    return Response('Success', 200)
+
 
 if __name__ == '__main__':
     app.run()
