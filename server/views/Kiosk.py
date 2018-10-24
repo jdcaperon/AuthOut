925642180491c91cc6014bed145859e6b236927a
Ryan <ryankurz123@gmail.com>
2018-10-24 13:10:53 +1000

    [Server] why

--- server/views/Kiosk.py
+++ server/views/Kiosk.py
@@ -56,18 +56,17 @@ def generate_code_endpoint():
     local_number = "0" + body[3:]
     parent = db.session.query(ParentModel).filter_by(mobile_number=local_number)
     parent_id = parent.first().as_dict()["id"]
-    resp.message("Parent id is " +str(parent_id))
     if parent.count() == 1:
-        try:
-            # here we generate a code for them and add it to the Db
+        # here we generate a code for them and add it to the Db
+        code = randint(1000, 9999)
+        available_code = db.session.query(OTPModel).filter_by(code=code)
+
+        # find a new code if it's not unique
+        while available_code.count() != 0:
             code = randint(1000, 9999)
             available_code = db.session.query(OTPModel).filter_by(code=code)
 
-            # find a new code if it's not unique
-            while available_code.count() != 0:
-                code = randint(1000, 9999)
-                available_code = db.session.query(OTPModel).filter_by(code=code)
-
+        try:
             current_code_location = db.session.query(OTPModel).filter_by(parent_id=parent_id)
             db.session.delete(current_code_location.first())
             db.session.commit()
